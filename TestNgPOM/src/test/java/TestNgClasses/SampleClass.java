17a848595758e278788c0849d8068bb2cd7cab07
HarishMandula <harishraj14384@gmail.com>
2025-12-11 14:37:16 +0530

    1st commit

---
+++ TestNgPOM/src/test/java/TestNgClasses/SampleClass.java
@@ -0,0 +1,11 @@
+package TestNgClasses;
+
+import org.testng.annotations.Test;
+
+public class SampleClass {
+
+    @Test
+    public void test(){
+        System.out.println("This is a test");
+    }
+}
